6db06fa4a8a792680007b2132b4adbb761e0504c
Santiago Douton <douton.santiago@gmail.com>
2021-12-07 22:17:33 -0300

    Trabajo js

---
+++ app.js
@@ -0,0 +1,88 @@
+/*Ejercicio 0
+var nombre = prompt ("¿cuál es tu nombre?");
+alert ("hola " + nombre);
+var apellido = prompt ("¿cuál es tu apellido?");
+alert ("hola " + nombre + " " + apellido);
+*/
+
+/*Ejercicio 1
+let numero1 = prompt ("Ingrese el primer número");
+let numero2 = prompt ("Ingrese el segundo número");
+let resultado = parseFloat (numero1) + parseFloat (numero2);
+alert ("El resultado de la suma es: " + resultado);
+*/
+
+/*Ejercicio 2
+let edad = prompt ("¿Qué edad tenés?");
+let resultado = 2021 - parseFloat (edad);
+alert ("Usted nació en " + resultado);
+*/
+
+/*Ejercicio 3
+let dineroInicial = 1000;
+alert ("Usted tiene $" + dineroInicial);
+let dineroRetirar = prompt ("¿Cuánto dinero quiere retirar?");
+let resultado = dineroInicial - parseFloat (dineroRetirar);
+alert ("Usted tiene $" + resultado);
+*/
+
+/*Ejercicio 4
+let nota1 = prompt ("Ingrese nota 1");
+let nota2 = prompt ("Ingrese nota 2");
+let nota3 = prompt ("Ingrese nota 3");
+let resultado = (parseFloat (nota1) + parseFloat (nota2) + parseFloat (nota3)) / 3;
+alert ("Resultado " + resultado);
+*/
+
+/*Ejercicio 5
+let metros = prompt ("Ingrese medida en metros");
+let pie = 3.28084 * parseFloat (metros);
+let pulgadas = 39.3701 * parseFloat (metros);
+let centimetros = 100 * parseFloat (metros);
+
+alert ("Pies = " + pie.toFixed (2) + "\nPulgadas = " + pulgadas.toFixed (2) + "\nCentimetros = " + centimetros.toFixed (2));
+*/
+
+/*Ejercicio 6
+let numero1 = prompt ("Ingrese el primer número");
+let numero2 = prompt ("Ingrese el segundo número");
+let resultadoSuma = parseFloat (numero1) + parseFloat (numero2);
+alert ("El resultado de la suma es: " + resultadoSuma);
+let numero3 = prompt ("Ingrese el tercer número");
+let resultadoMultiplicacion =resultadoSuma * parseFloat (numero3);
+alert ("El resultado de la multiplicación es: " + resultadoMultiplicacion);
+*/
+
+/*Ejercicio 7
+let minutos = prompt ("Ingrese los minutos");
+let segundos = parseFloat (minutos)*60;
+alert ("La cantidad de segundos a la que equivale: " + segundos);
+*/
+
+/*Ejercicio 8
+let altura = prompt ("¿Cuál es la medida de la altura?");
+let base = prompt ("¿Cuál es la medida de la base?");
+let area = parseFloat (base) * parseFloat (altura);
+alert ("Resultado: " + area);
+*/
+
+/*Ejercicio 9
+let altura = prompt ("¿Cuál es la medida de la altura?");
+let base = prompt ("¿Cuál es la medida de la base?");
+let area = parseFloat (base) * parseFloat (altura) /2;
+alert ("Resultado: " + area);
+*/
+
+/*Ejercicio 10
+let monto = prompt ("Ingrese monto: ");
+let descuento = prompt ("Ingrese descuento: ");
+let resultado = parseFloat (monto) - (parseFloat (monto) * parseFloat (descuento) /100);
+alert ("Precio final con descuento: " + resultado);
+*/
+
+
+
+
+
+
+
